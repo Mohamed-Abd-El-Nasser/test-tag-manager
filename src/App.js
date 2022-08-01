231f5176117aa488fe0bda499e6e7f83801735f4
mohamed shafei <abdm90486@gmail.com>
2022-08-01 22:49:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState, useRef } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const ref = useRef();
+    const value = `
+    
+    <script>
+        window.GbLoadInit = function () {
+            GbSdk.init({
+                APIKey: '07edad92b86d4200ad2a13258dd1b0f2',
+                lang: 'en',
+                playerUniqueId: 'cl5qjkr30000r01l16s3ehs8p',
+                playerAttributes: {},
+            });
+        };
+        console.log('init gameball');
+    </script>
+    <script
+        defer
+        src="https://assets.gameball.co/widget/js/gameball-init.min.js"
+    ></script>
+    `;
+    // const value = `<h1>Helloooo</h1>`;
+    useEffect(() => {
+        const parsedHTML = document
+            .createRange()
+            .createContextualFragment(value);
+        ref.current.appendChild(parsedHTML);
+        console.log('LLLLL', ref.current);
+    }, []);
+    return (
+        // <iframe
+        //     // width={200}
+        //     style={{ height: '100vh', width: '100%' }}
+        //     srcDoc={value}
+        //     ref={ref}
+        // />
+        <div ref={ref} style={{ height: '500px' }} />
+    );
 }
 
 export default App;
