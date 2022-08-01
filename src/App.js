c4b43daaac0366752bc56165a1dab6c7de3d9107
mohamed shafei <abdm90486@gmail.com>
2022-08-01 23:13:14 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -3,33 +3,33 @@ import './App.css';
 import React, { useEffect, useState, useRef } from 'react';
 
 function App() {
-    const ref = useRef();
-    const value = `
-    
-    <script>
-        window.GbLoadInit = function () {
-            GbSdk.init({
-                APIKey: '07edad92b86d4200ad2a13258dd1b0f2',
-                lang: 'en',
-                playerUniqueId: 'cl5qjkr30000r01l16s3ehs8p',
-                playerAttributes: {},
-            });
-        };
-        console.log('init gameball');
-    </script>
-    <script
-        defer
-        src="https://assets.gameball.co/widget/js/gameball-init.min.js"
-    ></script>
-    `;
+    // const ref = useRef();
+    // const value = `
+
+    // <script>
+    //     window.GbLoadInit = function () {
+    //         GbSdk.init({
+    //             APIKey: '07edad92b86d4200ad2a13258dd1b0f2',
+    //             lang: 'en',
+    //             playerUniqueId: 'cl5qjkr30000r01l16s3ehs8p',
+    //             playerAttributes: {},
+    //         });
+    //     };
+    //     console.log('init gameball');
+    // </script>
+    // <script
+    //     defer
+    //     src="https://assets.gameball.co/widget/js/gameball-init.min.js"
+    // ></script>
+    // `;
     // const value = `<h1>Helloooo</h1>`;
-    useEffect(() => {
-        const parsedHTML = document
-            .createRange()
-            .createContextualFragment(value);
-        ref.current.appendChild(parsedHTML);
-        console.log('LLLLL', ref.current);
-    }, []);
+    // useEffect(() => {
+    //     const parsedHTML = document
+    //         .createRange()
+    //         .createContextualFragment(value);
+    //     ref.current.appendChild(parsedHTML);
+    //     console.log('LLLLL', ref.current);
+    // }, []);
     return (
         // <iframe
         //     // width={200}
@@ -37,7 +37,8 @@ function App() {
         //     srcDoc={value}
         //     ref={ref}
         // />
-        <div ref={ref} style={{ height: '500px' }} />
+        // <div ref={ref} style={{ height: '500px' }} />
+        <div></div>
     );
 }
 
