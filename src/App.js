346084514ab2fb2dd1ad8d44baee68aa1ec3fc9d
mohamed shafei <abdm90486@gmail.com>
2022-08-02 19:22:34 +0200

    test9

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import React, { useEffect, useState, useRef } from 'react';
 function App() {
     const ref = useRef();
     const value = `
-
-    
         window.GbLoadInit = function () {
             GbSdk.init({
                 APIKey: '07edad92b86d4200ad2a13258dd1b0f2',
@@ -16,31 +14,40 @@ function App() {
             });
         };
         console.log('init gameball');
-    
-    
     `;
 
     // const value = `<h1>Helloooo</h1>`;
-    // useEffect(() => {
-    //     // const parsedHTML = document
-    //     //     .createRange()
-    //     //     .createContextualFragment(value);
-    //     // ref.current.appendChild(parsedHTML);
-    //     // console.log('LLLLL', ref.current);
-    //     const script = document.createElement('script');
-    //     script.innerText = value;
-    //     document.body.appendChild(script);
-    // }, []);
-    window.GbLoadInit = function () {
-        // eslint-disable-next-line no-undef
-        GbSdk.init({
-            APIKey: '07edad92b86d4200ad2a13258dd1b0f2',
-            lang: 'en',
-            playerUniqueId: 'cl5qjkr30000r01l16s3ehs8p',
-            playerAttributes: {},
-        });
-    };
-    console.log('init gameball');
+    useEffect(() => {
+        //     // const parsedHTML = document
+        //     //     .createRange()
+        //     //     .createContextualFragment(value);
+        //     // ref.current.appendChild(parsedHTML);
+        //     // console.log('LLLLL', ref.current);
+        const script = document.createElement('script');
+        script.innerText = value;
+        document.body.appendChild(script);
+    }, []);
+    // window.GbLoadInit = function () {
+    //     // eslint-disable-next-line no-undef
+    //     GbSdk.init({
+    //         APIKey: '07edad92b86d4200ad2a13258dd1b0f2',
+    //         lang: 'en',
+    //         playerUniqueId: 'cl5qjkr30000r01l16s3ehs8p',
+    //         playerAttributes: {},
+    //     });
+    //     if (typeof GbSdk === 'undefined') {
+    //         console.log('GbLoadInit is undefined', typeof GbSdk);
+    //     } else {
+    //         console.log('yesssssssss its successss', typeof GbSdk);
+    //     }
+    // };
+
+    // if (typeof window.GbLoadInit === 'undefined') {
+    //     console.log('GbLoadInit is undefined');
+    // } else {
+    //     console.log('yesssssssss its successss', typeof window.GbLoadInit);
+    // }
+
     return (
         // <iframe
         //     // width={200}
