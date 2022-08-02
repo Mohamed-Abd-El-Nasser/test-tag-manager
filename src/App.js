001251d7e700c94a1d98b12df23954b3e2354292
mohamed shafei <abdm90486@gmail.com>
2022-08-02 14:27:05 +0200

    test5

--- src/App.js
+++ src/App.js
@@ -3,33 +3,30 @@ import './App.css';
 import React, { useEffect, useState, useRef } from 'react';
 
 function App() {
-    // const ref = useRef();
-    // const value = `
+    const ref = useRef();
+    const value = `
 
-    // <script>
-    //     window.GbLoadInit = function () {
-    //         GbSdk.init({
-    //             APIKey: '07edad92b86d4200ad2a13258dd1b0f2',
-    //             lang: 'en',
-    //             playerUniqueId: 'cl5qjkr30000r01l16s3ehs8p',
-    //             playerAttributes: {},
-    //         });
-    //     };
-    //     console.log('init gameball');
-    // </script>
-    // <script
-    //     defer
-    //     src="https://assets.gameball.co/widget/js/gameball-init.min.js"
-    // ></script>
-    // `;
+    <script>
+        window.GbLoadInit = function () {
+            GbSdk.init({
+                APIKey: '07edad92b86d4200ad2a13258dd1b0f2',
+                lang: 'en',
+                playerUniqueId: 'cl5qjkr30000r01l16s3ehs8p',
+                playerAttributes: {},
+            });
+        };
+        console.log('init gameball');
+    </script>
+    
+    `;
     // const value = `<h1>Helloooo</h1>`;
-    // useEffect(() => {
-    //     const parsedHTML = document
-    //         .createRange()
-    //         .createContextualFragment(value);
-    //     ref.current.appendChild(parsedHTML);
-    //     console.log('LLLLL', ref.current);
-    // }, []);
+    useEffect(() => {
+        const parsedHTML = document
+            .createRange()
+            .createContextualFragment(value);
+        ref.current.appendChild(parsedHTML);
+        console.log('LLLLL', ref.current);
+    }, []);
     return (
         // <iframe
         //     // width={200}
@@ -37,8 +34,8 @@ function App() {
         //     srcDoc={value}
         //     ref={ref}
         // />
-        // <div ref={ref} style={{ height: '500px' }} />
-        <div></div>
+        <div ref={ref} style={{ height: '500px' }} />
+        //<div></div>
     );
 }
 
